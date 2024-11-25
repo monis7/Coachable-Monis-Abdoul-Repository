fb778745763b0cc10eaab1eadf27a21490442563
Mohammed Abdoul <Monis@MacBookPro.lan>
2024-11-25 12:36:28 -0500

    209 Minimum Size Subarray Sum

---
+++ 209_Minimum_Size_Subarray_Sum.py
@@ -0,0 +1,18 @@
+"""209 Minimum Size Subarray Sum"""
+
+from typing import List
+
+class Solution:
+    """Solution Class"""
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+       min_length = float('inf')
+       current_sum = 0
+       start = 0
+       for end in range(len(nums)):
+           current_sum += nums[end]
+           while current_sum >= target:
+               min_length = min(min_length, end - start + 1)
+               current_sum -= nums[start]
+               start += 1
+       return min_length if min_length != float('inf') else 0
+
