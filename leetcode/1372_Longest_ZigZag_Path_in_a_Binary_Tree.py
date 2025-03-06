6a59ff6df06498bccbc447d45935863743ad80cc
monis7 <90949169+monis7@users.noreply.github.com>
2025-03-06 00:56:33 -0500

    Update 1372_Longest_ZigZag_Path_in_a_Binary_Tree.py

--- leetcode/1372_Longest_ZigZag_Path_in_a_Binary_Tree.py
+++ leetcode/1372_Longest_ZigZag_Path_in_a_Binary_Tree.py
@@ -8,13 +8,12 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        def dfs(node, direction, length):
-            if not node:
-                return length - 1
-            if direction == 'left':
-                return max(dfs(node.left, 'right', length + 1), dfs(node.right, 'left', 1))
-            else:
-                return max(dfs(node.right, 'left', length + 1), dfs(node.left, 'right', 1))
-
-        return dfs(root, 'left', 0)
+        def longestZigZag(self, root: Optional[TreeNode]) -> int:
+            def dfs(node, is_left: bool, length: int) -> int:
+                if not node:
+                    return length - 1
+                if is_left:
+                    return max(dfs(node.left, False, length + 1), dfs(node.right, True, 1))
+                else:
+                    return max(dfs(node.right, True, length + 1), dfs(node.left, False, 1))
+            return dfs(root, True, 0)
