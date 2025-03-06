21e9f4264c91d5e50431d202713da2013e45752f
monis7 <90949169+monis7@users.noreply.github.com>
2025-03-06 01:05:55 -0500

    Update 429_Nary_Tree_Level_Order_Traversal.py

--- leetcode/429_Nary_Tree_Level_Order_Traversal.py
+++ leetcode/429_Nary_Tree_Level_Order_Traversal.py
@@ -10,11 +10,11 @@ class Solution:
     def levelOrder(self, root: Node) -> List[List[int]]:
         if not root:
             return []
-        result, queue = [], [root]
+        result, queue = [], deque([root])
         while queue:
             level = []
             for _ in range(len(queue)):
-                node = queue.pop(0)
+                node = queue.popleft()
                 level.append(node.val)
                 queue.extend(node.children)
             result.append(level)
